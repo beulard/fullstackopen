0e5bfb8aa05c2fb5eb761ff9ba22567792318ba5
Matthias Dubouchet <m.dubouchet18@imperial.ac.uk>
2023-01-06 19:20:00 +0100

    Finish part 2e

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import { useEffect, useState } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import './index.css'
+
+const Notification = ({message}) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
+const Footer = () => {
+  const style = {
+    color: 'blue',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={style}>
+      <br />
+      <em>Note app, beulard, 2023</em>
+    </div>
+  )
+}
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error')
 
   useEffect(() => {
     noteService
@@ -43,7 +70,10 @@ const App = (props) => {
         setNotes(notes.map(note => note.id !== id ? note : updatedNote))
       })
       .catch(error => {
-        alert(`the note ${note.content} was already deleted from the server`)
+        setErrorMessage(`the note ${note.content} was already deleted from the server`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -53,6 +83,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
       </div>
@@ -65,6 +96,7 @@ const App = (props) => {
         <input value={newNote} onChange={event => setNewNote(event.target.value)} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
--- src/services/notes.js
+++ src/services/notes.js
@@ -3,7 +3,7 @@ const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
-    return request.then(response => response.data)
+    return request.then(response => response.data.concat({ id: 1000, content: "hello world", date: "2022-1-17T17:30:31.098Z", important: true }))
 }
 
 const create = newObject => {
