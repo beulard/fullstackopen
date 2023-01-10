a4138ee491e90099994a5fdca719b99e3bb4fa35
Matthias Dubouchet <m.dubouchet18@imperial.ac.uk>
2023-01-10 21:43:44 +0100

    Combine frontend and backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
@@ -83,7 +84,7 @@ app.put('/api/notes/:id', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
