81ecdde6646e7e91ae7fb2ccce6ae43f9d8316cc
Matthias Dubouchet <m.dubouchet18@imperial.ac.uk>
2023-01-11 11:03:51 +0100

    Integrate MongoDB instance

--- index.js
+++ index.js
@@ -1,67 +1,59 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
+const Note = require('./models/note')
 
 app.use(express.static('build'))
 app.use(express.json())
-app.use(cors())
-
-let notes = [{ id: 1, content: "HTML is not easy", date: "2022-05-30T17:30:31.098Z", important: true }, { id: 2, content: "Browser can execute only Javascript", date: "2022-05-30T18:39:34.091Z", important: false }, { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2022-05-30T19:20:14.298Z", important: true }]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello world</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end(`note ${id} not found`)
-    }
+    })
+    .catch(err => {
+        response.status(404).send(err.message)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
-
-    if (!request.body.content) {
+    const body = request.body
+    if (request.body === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: request.body.content,
         important: request.body.important || false,
-        date: new Date(),
-        id: generateId()
-    }
-    
-    notes = notes.concat(note)
+        date: new Date()
+    })
 
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.put('/api/notes/:id', (request, response) => {
     const body = request.body
-    
+
     if (!body.content || body.important === null) {
         return response.status(400).json({
             error: 'content or important missing'
@@ -80,7 +72,7 @@ app.put('/api/notes/:id', (request, response) => {
     }
 
     notes = notes.map(n => n.id === id ? note : n)
-    
+
     response.json(note)
 })
 
